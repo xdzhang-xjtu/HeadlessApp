24209aba91da84ef10dcdf8762b26541d981f818
zhangxiaodong10 <zhangxiaodong10@baidu.com>
2018-06-01 20:39:05 +0800

    second

--- src/com/baidu/xdzhang/HeadlessApplication.java
+++ src/com/baidu/xdzhang/HeadlessApplication.java
@@ -1,5 +1,9 @@
 package com.baidu.xdzhang;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 
@@ -8,6 +12,14 @@ public class HeadlessApplication implements IApplication {
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
 		System.out.println("Hello from Eclipse application");
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IWorkspaceRoot root = workspace.getRoot();
+		// Get all projects in the workspace
+		IProject[] projects = root.getProjects();
+		// Loop over all projects
+		for (IProject project : projects) {
+			System.out.println(project.getName());
+		}
 		return null;
 	}
 
